ba078e503fdef162c45c57854f83e68bf6c19a88
terifan <pinjata69@gmail.com>
2018-03-08 21:37:12 +0100

    update

--- src/org/terifan/compression/basic_arithmetic/Test.java
+++ src/org/terifan/compression/basic_arithmetic/Test.java
@@ -4,6 +4,7 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.Arrays;
+import org.terifan.compression.bwt.BWT;
 import org.terifan.compression.io.BitInputStream;
 import org.terifan.compression.io.BitOutputStream;
 
@@ -15,11 +16,19 @@ public class Test
 		try
 		{
 			test("aabaabaacaaabbaababaaaacaabababaaaaaacabbbbabaaaaba".getBytes(), 3, 'a', false);
-			
-			String s = "big biggles biggeled olbig bigling boggled bigs";
 
-			test((s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s).getBytes(), 256, 0, false);
-			test((s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s).getBytes(), 256, 0, true);
+//			String s = "big biggles biggeled olbig bigling boggled bigs";
+//			byte[] in = (s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s+s).getBytes();
+
+			byte[] in = "A number of optimizations can make these algorithms run more efficiently without changing the output. There is no need to represent the table in either the encoder or decoder. In the encoder, each row of the table can be represented by a single pointer into the strings, and the sort performed using the indices. Some care must be taken to ensure that the sort does not exhibit bad worst-case behavior: Standard library sort functions are unlikely to be appropriate. In the decoder, there is also no need to store the table, and in fact no sort is needed at all. In time proportional to the alphabet size and string length, the decoded string may be generated one character at a time from right to left. A \"character\" in the algorithm can be a byte, or a bit, or any other convenient size. There is no need to have an actual 'EOF' character. Instead, a pointer can be used that remembers where in a string the 'EOF' would be if it existed. In this approach, the output of the BWT must include both the transformed string, and the final value of the pointer. That means the BWT does expand its input slightly. The inverse transform then shrinks it back down to the original size: it is given a string and a pointer, and returns just a string. A complete description of the algorithms can be found in Burrows and Wheeler's paper[4][citation needed], or in a number of online sources. When a bijective variant of the Burrows-Wheeler transform is performed on \"^BANANA\", you get ANNBAA^ without the need for a special character for the end of the string. A special character forces one to increase character space by one, or to have a separate field with a numerical value for an offset. Either of these features makes data compression more difficult. When dealing with short files, the savings are great percentage-wise. The bijective transform is done by sorting all rotations of the Lyndon words. In comparing two strings of unequal length, one can compare the infinite periodic repetitions of each of these in lexicographic order and take the last column of the base-rotated Lyndon word. For example, the text \"^BANANA|\" is transformed into \"ANNBAA^|\" through these steps (the red | character indicates the EOF pointer) in the original string. The EOF character is unneeded in the bijective transform, so it is dropped during the transform and re-added to its proper place in the file.".getBytes();
+			
+			test(in, 256, 0, false);
+			test(in, 256, 0, true);
+
+			int index = BWT.encode(in);
+
+			test(in, 256, 0, false);
+			test(in, 256, 0, true);
 		}
 		catch (Throwable e)
 		{
--- src/org/terifan/compression/bwt/BWT.java
+++ src/org/terifan/compression/bwt/BWT.java
@@ -2,7 +2,6 @@ package org.terifan.compression.bwt;
 
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.concurrent.atomic.AtomicInteger;
 
 
 public class BWT
@@ -13,9 +12,11 @@ public class BWT
 		{
 			byte[] in = "A number of optimizations can make these algorithms run more efficiently without changing the output. There is no need to represent the table in either the encoder or decoder. In the encoder, each row of the table can be represented by a single pointer into the strings, and the sort performed using the indices. Some care must be taken to ensure that the sort does not exhibit bad worst-case behavior: Standard library sort functions are unlikely to be appropriate. In the decoder, there is also no need to store the table, and in fact no sort is needed at all. In time proportional to the alphabet size and string length, the decoded string may be generated one character at a time from right to left. A \"character\" in the algorithm can be a byte, or a bit, or any other convenient size. There is no need to have an actual 'EOF' character. Instead, a pointer can be used that remembers where in a string the 'EOF' would be if it existed. In this approach, the output of the BWT must include both the transformed string, and the final value of the pointer. That means the BWT does expand its input slightly. The inverse transform then shrinks it back down to the original size: it is given a string and a pointer, and returns just a string. A complete description of the algorithms can be found in Burrows and Wheeler's paper[4][citation needed], or in a number of online sources. When a bijective variant of the Burrows-Wheeler transform is performed on \"^BANANA\", you get ANNBAA^ without the need for a special character for the end of the string. A special character forces one to increase character space by one, or to have a separate field with a numerical value for an offset. Either of these features makes data compression more difficult. When dealing with short files, the savings are great percentage-wise. The bijective transform is done by sorting all rotations of the Lyndon words. In comparing two strings of unequal length, one can compare the infinite periodic repetitions of each of these in lexicographic order and take the last column of the base-rotated Lyndon word. For example, the text \"^BANANA|\" is transformed into \"ANNBAA^|\" through these steps (the red | character indicates the EOF pointer) in the original string. The EOF character is unneeded in the bijective transform, so it is dropped during the transform and re-added to its proper place in the file.".getBytes();
 
-			AtomicInteger oIndex = new AtomicInteger();			
-			byte[] en = encode(in, oIndex);
-			byte[] de = decode(en, oIndex.get());
+			byte[] en = in.clone();
+			int index = encode(en);
+
+			byte[] de = en.clone();
+			decode(de, index);
 
 			System.out.println(new String(en));
 			System.out.println(new String(de));
@@ -30,12 +31,12 @@ public class BWT
 	private static final int R = 256;
 
 
-	public static byte[] encode(byte[] s, AtomicInteger oIndex)
+	public static int encode(byte[] aBuffer)
 	{
-		int n = s.length;
+		int n = aBuffer.length;
 
-		byte[] tmp = Arrays.copyOfRange(s, 0, 2 * n);
-		System.arraycopy(s, 0, tmp, n, n);
+		byte[] tmp = Arrays.copyOfRange(aBuffer, 0, 2 * n);
+		System.arraycopy(aBuffer, 0, tmp, n, n);
 		byte[][] rotatedStrings = new byte[n][];
 		for (int i = 0; i < n; i++)
 		{
@@ -47,27 +48,25 @@ public class BWT
 		int index = 0;
 		for (int i = 0; i < rotatedStrings.length; i++)
 		{
-			if (Arrays.equals(rotatedStrings[i], s))
+			if (Arrays.equals(rotatedStrings[i], aBuffer))
 			{
 				index = i;
 				break;
 			}
 		}
 
-		oIndex.set(index);
-
-		byte[] out = new byte[n];
 		for (int i = 0; i < n; i++)
 		{
-			out[i] = rotatedStrings[i][n - 1];
+			aBuffer[i] = rotatedStrings[i][n - 1];
 		}
-		return out;
+		
+		return index;
 	}
 
 
-	public static byte[] decode(byte[] aInput, int aIndex)
+	public static void decode(byte[] aBuffer, int aIndex)
 	{
-		int n = aInput.length;
+		int n = aBuffer.length;
 
 		int[] next = new int[n];
 		byte[] symbols = new byte[n];
@@ -75,7 +74,7 @@ public class BWT
 
 		for (int i = 0; i < n; i++)
 		{
-			count[aInput[i] + 1]++;
+			count[aBuffer[i] + 1]++;
 		}
 		for (int r = 0; r < R; r++)
 		{
@@ -83,19 +82,15 @@ public class BWT
 		}
 		for (int i = 0; i < n; i++)
 		{
-			next[count[aInput[i]]] = i;
-			symbols[count[aInput[i]]++] = aInput[i];
+			next[count[aBuffer[i]]] = i;
+			symbols[count[aBuffer[i]]++] = aBuffer[i];
 		}
-
-		byte[] out = new byte[n];
 
 		for (int i = 0; i < n; i++)
 		{
-			out[i] = symbols[aIndex];
+			aBuffer[i] = symbols[aIndex];
 			aIndex = next[aIndex];
 		}
-		
-		return out;
 	}
 
 
